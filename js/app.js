ab2bc4bf392afc5a9423cc2001ae665c5f7b8bf8
vishwas.sharma <vishwas.sharma@thoughtspot.com>
2016-12-20 00:50:29 -0800

    First successful index and query round trip.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,46 @@
-var app = angular.module('faqEngine', []);
-app.controller("MyC", function ($scope) {
-  this.product = "Hello world !";
+var app = angular.module('faqEngine', ['elasticsearch'], ['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode(true);
+  }]
+);
 
-  $scope.search = function (msg) {
-    alert(msg);
+app.service('client', function (esFactory) {
+  return esFactory({
+    host: 'localhost:9200',
+    apiVersion: '2.3',
+    log: 'trace'
+  });
+});
+
+app.controller("MyC", function($scope, client) {
+
+  $scope.search = function (query) {
+    var query = {
+      match: {
+        _all: query
+      }
+    };
+
+    client.search({
+      index: 'faqs',
+      type: 'faq',
+      body: {
+        query: query
+      }
+    });
   }
 
   $scope.submit = function (title, details) {
-    alert(title + " " + details);
+    client.index({
+      index: 'faqs',
+      type: 'faq',
+      body: {
+        title: title,
+        details: details,
+      }
+    }, function (error, response) {
+      if (!error) {
+        console.log("Success !!!");
+      }
+    });
   }
 });
